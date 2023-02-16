0970b610c26057b3dd855fb3c5d0ef12b1b9a316
MINGUOLIU <66464677+MINGUOLIU@users.noreply.github.com>
2023-02-16 15:39:10 +0800

    第二次提交时间是15.38分

---
+++ 925.长按键入.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=925 lang=cpp
+ *
+ * [925] 长按键入
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    bool isLongPressedName(string name, string typed)
+    {
+        int i = 0, j = 0;
+        while (j < typed.length())
+        {
+            if (i < name.length() && name[i] == typed[j])
+            {
+                i++;
+                j++;
+            }
+            else if (j > 0 && typed[j] == typed[j - 1])
+            {
+                j++;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return i == name.length();
+    }
+};
+// @lc code=end

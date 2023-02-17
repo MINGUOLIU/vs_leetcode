1bf04eb72575ad56bd56050f48d598884ce3da1b
MINGUOLIU <66464677+MINGUOLIU@users.noreply.github.com>
2023-02-17 21:19:58 +0800

    现在是晚上九点二十分的递交

---
+++ 2月17/1032.字符流.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=1032 lang=cpp
+ *
+ * [1032] 字符流
+ */
+
+// @lc code=start
+class StreamChecker {
+public:
+    StreamChecker(vector<string>& words) {
+
+    }
+    
+    bool query(char letter) {
+
+    }
+};
+
+/**
+ * Your StreamChecker object will be instantiated and called as such:
+ * StreamChecker* obj = new StreamChecker(words);
+ * bool param_1 = obj->query(letter);
+ */
+// @lc code=end
+

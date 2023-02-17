d0afe86d4936bc067b8eb572f214ddcef283fbc1
MINGUOLIU <66464677+MINGUOLIU@users.noreply.github.com>
2023-02-17 21:27:01 +0800

    更改正则表达式的内容

--- 2月16/10.正则表达式匹配.cpp
+++ 2月16/10.正则表达式匹配.cpp
@@ -5,38 +5,44 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    bool isMatch(string s, string p) {
-        int m  =s.size();
+    bool isMatch(string s, string p)
+    {
+        int m = s.size();
         int n = p.size();
 
-        auto matches = [&](int i,int j)
+        auto matches = [&](int i, int j)
         {
-            if(i==0 ) return false;   
-            if(p[j-1]=='.') return true;
-            return s[i-1]==p[j-1];
+            if (i == 0)
+                return false;
+            if (p[j - 1] == '.')
+                return true;
+            return s[i - 1] == p[j - 1];
         };
 
-    vector <vector<int> > f(m+1,vector<int>(n+1));
-    f[0][0]=true;
-    for(int i=0;i<=m;i++)
-    {
-        for(int j=1;j<=n;++j)
+        vector<vector<int>> f(m + 1, vector<int>(n + 1));
+        f[0][0] = true;
+        for (int i = 0; i <= m; i++)
         {
-            if(p[j-1]=='*')
+            for (int j = 1; j <= n; ++j)
             {
-                f[i][j]|=f[i][j-2];
-                if(matches(i,j-1 )) f[i][j]|=f[i-1][j];
-            }
-            else
-            {
-                if(matches(i,j)) f[i][j]|=f[i-1][j-1];
+                if (p[j - 1] == '*')
+                {
+                    f[i][j] |= f[i][j - 2];
+                    if (matches(i, j - 1))
+                        f[i][j] |= f[i - 1][j];
+                }
+                else
+                {
+                    if (matches(i, j))
+                        f[i][j] |= f[i - 1][j - 1];
+                }
             }
         }
-    }
-    return f[m][n];
+        return f[m][n];
     }
 };
-// @lc code=end
-
+2月17日
+    // @lc code=end
